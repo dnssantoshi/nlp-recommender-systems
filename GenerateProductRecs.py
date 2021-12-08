c45f39093de869e8a5fb5a6cc0fbf505a54f4a48
Santoshi Diddi <dns.santoshi@gmail.com>
2021-12-08 15:26:17 -0600

    update

--- GenerateProductRecs.py
+++ GenerateProductRecs.py
@@ -29,7 +29,7 @@ def load_data():
     df = pd.read_pickle('df.pkl')
     indices = pd.read_pickle('indices.pkl')
 
-    # loading the pickle files from google cloud as these files are too large to commit to Github.
+    # loading the pickle files from google cloud storage as these files are too large to commit to Github.
     cosine_sim = load_cosines()
 
     return df, cosine_sim, indices
@@ -190,48 +190,59 @@ class GenerateProductRecs:
                 cols = st.beta_columns(col_size)
                 for j in range(0, col_size):
                     cols[j].image(product_images.values[i][j], width=75)
-                st.write("</br>",unsafe_allow_html=True)
+                st.write("</br>", unsafe_allow_html=True)
             self.pretty_print(product_titles.values[i], product_asin.values[i], product_desc.values[i],
                               product_images.values[i][0])
 
         return df['productTitle'].iloc[product_indices]
-        print("\nCompleted!")
 
     def technical_overview(self):
         st.sidebar.write("Technical overview")
-        st.sidebar.radio("Abstract")
         technical = st.sidebar.radio("", (
-        "Abstract", "Data Wrangling", "Text Embeddings", "Cosine Similarities", "Recommendations", "Reports"))
+            "Abstract", "Data Wrangling", "Text Embeddings", "Cosine Similarities", "Recommendations", "Reports"))
 
         if technical == 'Abstract':
+            st.subheader("Abstract")
             st.write(
                 "This application recommends products that are similar to a particular product. To acheive this, computed pairwise cosine similarity scores for all products based on the descriptions and brand attributes. The recommendation is products is based on this similarity score threshold.")
 
         if technical == 'Data Wrangling':
-            st.write("The dataset has been wrangled before using this for creating word vectors. This include imputing any missing values, creating any aggregated attributes, removing stopwords and punctuations and retrieving the important keywords for creating word vectors.")
+            st.subheader("Data Wrangling")
+            st.write(
+                "The dataset has been wrangled before using this for creating word vectors. This include imputing any missing values, creating any aggregated attributes, removing stopwords and punctuations and retrieving the important keywords for creating word vectors.")
 
         if technical == "Text Embeddings":
+            st.subheader("Text Embeddings")
             st.write(
                 'This is a natural language processing problem and we cannot use the raw text to compute similarities. So inorder to extract the same kind of featured products the word vectors of each description. Word vectors are the vectorized description of words in a document. The vectors carry the semantic meaning with it.')
 
+        if technical == 'Cosine Similarities':
+            st.subheader("Cosine Similarities")
+            st.write(
+                "Used cosine similarity to calculate the numeric quantity that denotes the similarity between two products. Used cosine similarity score is independent of the magnitude and it is easy and fast to calculate when compared to TF-IDF. This would return an matric of n*n where n is the total number of products. So each product will have a 1 x n mappings.")
+
         if technical == "Recommendations":
+            st.subheader("Product Recommendations")
             st.write('1. Get the index of the product based on the product title from the user')
             st.write('2. Retrieve the cosine similarity scores for that particular product with all products')
             st.write('3. Enumerate with the position and the similarity score')
-            st.write('4. Get the top n recommendations where n is the user input for how many recommendations the user wants to retrieve')
+            st.write(
+                '4. Get the top n recommendations where n is the user input for how many recommendations the user wants to retrieve')
             st.write('5. Return the products corresponding to the indexes of the top recommendations')
 
         if technical == "Reports":
             st.subheader("Product Metadata")
-            st.write("This reflects all the product metadata along with additional attributes such as price, brand, shipping information. This also provides the capability to search for a particular product based on ASIN, Brand and Item number. It also allows the user to export the data displayed on this table.")
+            st.write(
+                "This reflects all the product metadata along with additional attributes such as price, brand, shipping information. This also provides the capability to search for a particular product based on ASIN, Brand and Item number. It also allows the user to export the data displayed on this table.")
             st.subheader("User Reviews Dataset")
-            st.write("This is a data dump of all the user reviews in the amazon luxury beauty product category. This provides additional information such as the product review image if any uploaded, review description, summary of the review etc.,This enables user to filter the data based on the ASIN, Reviewer Id and user rating. It also allows the user to export the data displayed on this table.")
+            st.write(
+                "This is a data dump of all the user reviews in the amazon luxury beauty product category. This provides additional information such as the product review image if any uploaded, review description, summary of the review etc.,This enables user to filter the data based on the ASIN, Reviewer Id and user rating. It also allows the user to export the data displayed on this table.")
             st.subheader("User Reviews Dashboard")
-            st.write("This dashboard provides all the trends and the statistics of all the user reviews provided to luxury beauty category products. Some of the important questions and the kind of trends in getting user reviews are depicted over time. It enables user to filter on the data time frame they would like to see the trends for. Apart from this, user can filter with ASIN and Rating attributes.")
+            st.write(
+                "This dashboard provides all the trends and the statistics of all the user reviews provided to luxury beauty category products. Some of the important questions and the kind of trends in getting user reviews are depicted over time. It enables user to filter on the data time frame they would like to see the trends for. Apart from this, user can filter with ASIN and Rating attributes.")
 
     def construct_app(self):
         # This is the main point for application start up which triggers the rest of the function calls
-
         st.markdown(
             """
             <style>
@@ -267,9 +278,12 @@ class GenerateProductRecs:
         )
 
         self.construct_menu_tabs()
-        # st.write(
-        #     '<style>body { margin: 0; font-family: Arial, Helvetica, sans-serif;} .header{margin: 0; background:white;padding: 0px 320px 0px 320px; position:fixed;top:0;}</style><div class="header" id="myHeader"><img src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/woodland-gardening-amazon-png-logo-vector-8.png" alt="Not Available" style="width:300px;height:80px;" /></div>',
-        #     unsafe_allow_html=True)
+        st.write(
+            '<style>body { margin: 0; font-family: Arial, Helvetica, sans-serif;} .header{margin: 0; background:white;padding: 0px 720px 0px 520px; '
+            'position:fixed;top:0;}</style><div class="header" id="myHeader">'
+            '<img src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/woodland-gardening-amazon-png-logo-vector-8.png" alt="Not Available" style="width:300px;height:80px;" />'
+            '</div>',
+            unsafe_allow_html=True)
 
         return self
 
